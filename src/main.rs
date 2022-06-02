7856921a42b14d11779e9a110306740343bac411
Nick Payne <nick@kurai.co.uk>
2022-06-02 11:33:30 +0100

    completable game

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::TextureQuery;
 
-const SQUARE_SIZE: usize = 60;
-const SQUARES_TO_REMOVE: usize = 40;
+const CELL_SIZE: usize = 60;
+const CELLS_TO_REMOVE: usize = 40;
 const START_OFFSET: usize = 30;
 
 macro_rules! rect(
@@ -17,8 +17,12 @@ macro_rules! rect(
     )
 );
 
+struct Cell {
+    pub is_given: bool,
+    pub value: u8
+}
 struct Puzzle {
-    pub squares: Vec<u8>
+    pub cells: Vec<Cell>
 }
 
 fn valid_chunk(input: &[u8]) -> bool {
@@ -27,7 +31,7 @@ fn valid_chunk(input: &[u8]) -> bool {
     unique == filled.len()
 }
 
-fn get_squares<F>(f: F) -> Vec<Vec<u8>> where
+fn get_cells<F>(f: F) -> Vec<Vec<u8>> where
     F: Fn(usize) -> Vec<u8> {
     let mut rows = Vec::with_capacity(9);
     for i in 0..9 {
@@ -40,42 +44,42 @@ fn get_squares<F>(f: F) -> Vec<Vec<u8>> where
 impl Puzzle {
     pub fn new() -> Self {
         Self {
-            squares: vec![]
+            cells: vec![]
         }
     }
 
 
     fn get_rows(&self) -> Vec<Vec<u8>> {
-        get_squares(|i| {
+        get_cells(|i| {
             let start = i * 9;
-            self.squares[start..start+9].to_vec()
+            self.cells[start..start+9].iter().map(|v| v.value).collect()
         })
     }
 
     fn get_columns(&self) -> Vec<Vec<u8>> {
-        get_squares(|i| {
-            let mut squares = vec![];
+        get_cells(|i| {
+            let mut cells = vec![];
             for row in 0..9 {
                 let idx = i + (row * 9);
-                squares.push(self.squares[idx]);
+                cells.push(self.cells[idx].value);
             }
-            squares
+            cells
         })
     }
 
     fn get_boxes(&self) -> Vec<Vec<u8>> {
-        get_squares(|i| {
+        get_cells(|i| {
             let start = ((i % 3) * 3) + (i / 3) * 27;
-            let mut squares = vec![];
+            let mut cells = vec![];
             for row in 0..3 {
                 let start= start + (row * 9);
-                squares.extend(&self.squares[start..start+3]);
+                cells.extend(self.cells[start..start+3].iter().map(|v| v.value));
             }
-            squares
+            cells
         })
     }
 
-    // are all rules satisfied when we ignore empty squares?
+    // are all rules satisfied when we ignore empty cells?
     pub fn is_valid(&self) -> bool {
         !(self.get_rows().iter().any(|row| !valid_chunk(row)) ||
             self.get_columns().iter().any(|col| !valid_chunk(col)) ||
@@ -83,7 +87,7 @@ impl Puzzle {
     }
 
     pub fn is_solved(&self) -> bool {
-        let filled = self.squares.iter().filter(|&v| *v != 0).copied().count();
+        let filled = self.cells.iter().filter(|&c| c.value != 0).count();
         self.is_valid() && filled == 81
     }
 }
@@ -92,13 +96,16 @@ fn generate_valid_puzzle() -> Puzzle {
     let mut rng = thread_rng();
     let mut puzzle = Puzzle::new();
     loop {
-        puzzle.squares = [0; 81].to_vec();
+        puzzle.cells = (0..81).map(|_| Cell {is_given: false, value: 0}).collect();
         for i in 0..81 {
-            let mut next_squares: Vec<u8> = (1..10).collect();
-            next_squares.shuffle(&mut rng);
+            let mut next_cell_values: Vec<u8> = (1..10).collect();
+            next_cell_values.shuffle(&mut rng);
 
-            for square in next_squares {
-                puzzle.squares[i] = square;
+            for value in next_cell_values {
+                puzzle.cells[i] = Cell {
+                    is_given: true,
+                    value
+                };
                 if puzzle.is_valid() {
                     break
                 }
@@ -109,11 +116,14 @@ fn generate_valid_puzzle() -> Puzzle {
         }
     }
 
-    let mut squares_to_remove: Vec<u8> = (0..81).collect();
-    squares_to_remove.shuffle(&mut rng);
-    for square in &squares_to_remove[0..SQUARES_TO_REMOVE] {
-        let idx = *square as usize;
-        puzzle.squares[idx] = 0;
+    let mut cells_to_clear: Vec<u8> = (0..81).collect();
+    cells_to_clear.shuffle(&mut rng);
+    for idx in &cells_to_clear[0..CELLS_TO_REMOVE] {
+        let idx = *idx as usize;
+        puzzle.cells[idx] = Cell {
+            is_given: false,
+            value: 0
+        };
     }
     puzzle
 }
@@ -123,7 +133,7 @@ fn main() -> Result<(), String> {
     let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
     let video_subsystem = sdl_context.video()?;
     let window = video_subsystem
-        .window("rudokn?", 800, 600)
+        .window("rudokn?", 600, 600)
         .position_centered()
         .build()
         .map_err(|e| e.to_string())?;
@@ -155,16 +165,22 @@ fn main() -> Result<(), String> {
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. } => break 'running,
+                Event::KeyDown { keycode: Some(Keycode::Escape), ..} => {
+                    selected_cell = None;
+                }
                 Event::KeyDown { keycode: Some(k), ..} => {
-                    if let Some(cell) = selected_cell {
-                        let num = k as i32;
-                        if num >= 49 && num <= 57 {
-                            puzzle.squares[cell] = (num - 48) as u8;
-                        } else if k == Keycode::Backspace {
-                            selected_cell = None;
-                            puzzle.squares[cell] = 0;
+                    if let Some(idx) = selected_cell {
+                        let given = puzzle.cells[idx].is_given;
+                        if !given {
+                            let num = k as i32;
+                            if num >= 49 && num <= 57 {
+                                puzzle.cells[idx].value = (num - 48) as u8;
+                            } else if k == Keycode::Backspace {
+                                selected_cell = None;
+                                puzzle.cells[idx].value = 0;
+                            }
+                            println!("Valid?: {} Solved?: {}", puzzle.is_valid(), puzzle.is_solved());
                         }
-                        println!("Valid?: {} Solved?: {}", puzzle.is_valid(), puzzle.is_solved());
                     }
                 }
                 _ => {}
@@ -176,8 +192,8 @@ fn main() -> Result<(), String> {
         let my = mouse_state.y() - START_OFFSET as i32;
 
         if mouse_state.left() {
-            let x = mx/SQUARE_SIZE as i32;
-            let y = my/SQUARE_SIZE as i32;
+            let x = mx/ CELL_SIZE as i32;
+            let y = my/ CELL_SIZE as i32;
             if x >= 0 && y >= 0 && x < 9 && y < 9 {
                 let idx = x + (y * 9);
                 selected_cell = Some(idx as usize);
@@ -187,41 +203,44 @@ fn main() -> Result<(), String> {
         canvas.set_draw_color(Color::RGB(100, 100, 100));
         canvas.clear();
         for i in 0..81 {
-            let square = puzzle.squares[i];
+            let value = puzzle.cells[i].value;
+            let given = puzzle.cells[i].is_given;
             let row = i/9;
             let column = i % 9;
-            let x = START_OFFSET + column * SQUARE_SIZE;
-            let y = START_OFFSET + row * SQUARE_SIZE;
-            let size = SQUARE_SIZE as u32;
+            let x = START_OFFSET + column * CELL_SIZE;
+            let y = START_OFFSET + row * CELL_SIZE;
+            let size = CELL_SIZE as u32;
             canvas.set_draw_color(Color::GREY);
             canvas.fill_rect(rect!(x,  y, size, size))?;
 
             let color = if Some(i) == selected_cell {
                 Color::YELLOW
-            } else if mx >= 0 && my >= 0 && mx/SQUARE_SIZE as i32 == column as i32 && my/SQUARE_SIZE as i32 == row as i32 {
+            } else if mx >= 0 && my >= 0 && mx/ CELL_SIZE as i32 == column as i32 && my/ CELL_SIZE as i32 == row as i32 {
                 Color::RGBA(255, 255, 200, 255)
+            } else if given {
+                Color::RGBA(240, 240, 240, 255)
             } else {
                 Color::WHITE
             };
             canvas.set_draw_color(color);
             canvas.fill_rect(rect!(x, y, size - 2, size - 2))?;
-            if square != 0 {
-                let (digit, w, h) = &digits[square as usize];
+            if value != 0 {
+                let (digit, w, h) = &digits[value as usize];
                 canvas.copy(digit, None, Some(rect!(x + 16, y - 2, *w, *h)))?;
             }
         }
         for vertical_stripes in 0..4 {
-            let x = START_OFFSET + (vertical_stripes * SQUARE_SIZE * 3);
+            let x = START_OFFSET + (vertical_stripes * CELL_SIZE * 3);
             let y = START_OFFSET;
             canvas.set_draw_color(Color::BLACK);
-            canvas.fill_rect(rect!(x-1, y, 2, 9 * SQUARE_SIZE))?;
+            canvas.fill_rect(rect!(x-1, y, 2, 9 * CELL_SIZE))?;
 
         }
         for horizontal_stripes in 0..4 {
             let x = START_OFFSET;
-            let y = START_OFFSET + (horizontal_stripes * SQUARE_SIZE * 3);
+            let y = START_OFFSET + (horizontal_stripes * CELL_SIZE * 3);
             canvas.set_draw_color(Color::BLACK);
-            canvas.fill_rect(rect!(x, y-1, 9 * SQUARE_SIZE, 2))?;
+            canvas.fill_rect(rect!(x, y-1, 9 * CELL_SIZE, 2))?;
 
         }
         canvas.present();
